fe02cac80d528234a73112dd50ebb9b841e37aac
pikaChin <derrfgh4563@gmail.com>
2019-09-18 16:29:21 +0800

    number of tickets complete

--- app/src/main/java/com/example/buyticket/MainActivity.java
+++ app/src/main/java/com/example/buyticket/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup ticketType;
+    private RadioGroup ticketNum;
     private TextView txv;
 
     @Override
@@ -19,16 +20,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ticketType = findViewById(R.id.ticketType);
         txv = findViewById(R.id.txv);
+        ticketNum = findViewById(R.id.ticketNum);
     }
 
     public void showType(View v) {
+        RadioButton select;
+        String num = "";
+
         if (ticketType.getCheckedRadioButtonId() == -1) {
             txv.setText("Please choose which type of ticket you want");
+            return;
         }
         else {
             int typeId = ticketType.getCheckedRadioButtonId();
-            RadioButton select = findViewById(typeId);
-            txv.setText("you buy a " + select.getText() + " ticket");
+            select = findViewById(typeId);
         }
+
+        if (ticketNum.getCheckedRadioButtonId() == -1) {
+            txv.setText("Please choose how many tickets you want");
+            return;
+        }
+        else if (ticketNum.getCheckedRadioButtonId() == R.id.buyOneTicket)
+            num = "1";
+        else if (ticketNum.getCheckedRadioButtonId() == R.id.buyTwoTicket)
+            num = "2";
+        else if (ticketNum.getCheckedRadioButtonId() == R.id.buyThreeTicket)
+            num = "3";
+        else if (ticketNum.getCheckedRadioButtonId() == R.id.buyFourTicket)
+            num = "4";
+
+
+        txv.setText("you buy " + num + " " + select.getText() + " tickets");
     }
 }
